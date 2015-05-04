6738024af6061af93bf51a057b48cb4025fc0b8f
Your Name <you@example.com>
2015-05-04 17:15:31 +0800

    palindrome excercise

---
+++ palindrome.rb
@@ -0,0 +1,10 @@
+#### Palindrome
+
+# Write a function to check if a given string is a palindrome
+
+
+def isPalindrome(string)
+  if string == string.reverse
+  	print true
+  end
+end 
---
+++ reverse.rb
@@ -0,0 +1,11 @@
+#### Reverse String
+
+# Write a function to reverse a string
+
+
+def reverse(string)
+ 		string.reverse
+end
+
+puts reverse("harry")
+puts reverse("My long string")
