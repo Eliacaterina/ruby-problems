d45c2aabf9f2147a0935ee2ab268a4fb49455d3e
Your Name <you@example.com>
2015-05-05 10:20:24 +0800

    random Array

---
+++ missingNumb.rb
@@ -0,0 +1,23 @@
+#### Missing Number
+
+# You have an array of numbers 1 to 100 in an array. Only one number is missing in the array. The array is unsorted. Find the missing number.
+
+
+#   array = []
+# 	i = 0
+# 	while i <= 100
+# 	    array << rand(100) 
+# 	    i += 1  
+# 	    puts array
+# 	    array.delete_at(rand(array.length))
+# 	end
+	
+# 	#I dont understand how to find the missing numb. 
+
+# missingNumber([])
+
+def missingNumber(array)
+  # your magic
+end
+
+missingNumber(1..100)
